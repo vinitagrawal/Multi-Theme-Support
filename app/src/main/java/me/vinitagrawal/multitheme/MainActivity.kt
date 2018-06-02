c661230ea133dc81f670536d74937cc59f51dd62
Vinit Agrawal <vinitagrawal91@gmail.com>
2018-06-02 20:49:56 +0530

    Check and set new theme in previous activity

--- app/src/main/java/me/vinitagrawal/multitheme/MainActivity.kt
+++ app/src/main/java/me/vinitagrawal/multitheme/MainActivity.kt
@@ -1,15 +1,25 @@
 package me.vinitagrawal.multitheme
 
-import android.support.v7.app.AppCompatActivity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var currentTheme: String
+  private lateinit var sharedPref: SharedPreferences
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+    currentTheme = sharedPref.getString(KEY_CURRENT_THEME, LILAC_THEME)
+    setAppTheme(currentTheme)
+
     setContentView(R.layout.activity_main)
   }
 
@@ -23,5 +33,19 @@ class MainActivity : AppCompatActivity() {
       startActivity(Intent(this, SettingsActivity::class.java))
 
     return super.onOptionsItemSelected(item)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    val selectedTheme = sharedPref.getString(KEY_CURRENT_THEME, LILAC_THEME)
+    if(currentTheme != selectedTheme)
+      recreate()
+  }
+
+  private fun setAppTheme(currentTheme: String) {
+    when (currentTheme) {
+      MINT_THEME -> setTheme(R.style.Theme_App_Mint)
+      else -> setTheme(R.style.Theme_App_Lilac)
+    }
   }
 }
